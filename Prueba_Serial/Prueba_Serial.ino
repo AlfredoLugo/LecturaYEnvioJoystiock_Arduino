f4a1d86d8c74b519fba2151c3df7a2b845dc1be8
Ing. Alfredo Lugo <alugo@markoptic.mx>
2016-12-08 10:13:41 -0700

    Versión_Inicial
    Versión incial del codigo

---
+++ Prueba_Serial/Prueba_Serial.ino
@@ -0,0 +1,22 @@
+#define ejeX A1
+#define ejeY A0
+#define seleccionMarcha A2
+
+int valorX;
+int valorY;
+int valorMarcha;
+
+void setup() {
+  Serial.begin(250000);}
+
+void loop() {
+  valorX = analogRead(ejeX);
+  valorY = analogRead(ejeY);
+  valorMarcha = analogRead(seleccionMarcha);
+  //Serial.println(valorX);
+  Serial.println(valorY);
+  //Serial.print(valorMarcha);
+  //Serial.println();
+  Serial.flush();
+  delay(500);
+}
